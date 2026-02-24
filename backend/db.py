b5f1d19e47743e8b791e494dd9195afe4a7111d8
ruo-edwin <ruoedwin@gmail.com>
2026-02-24 17:11:13 +0300

    Add pymysql driver

--- backend/db.py
+++ backend/db.py
@@ -10,6 +10,9 @@ env_path = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path=env_path)   
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+DATABASE_URL = DATABASE_URL.replace(
+    "mysql://", "mysql+pymysql://"
+)
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is missing or not set.")
